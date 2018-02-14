e7e5fe42e9a0db130f9410a950bf72f88f2a4b7a
yx2398 <yx2387@columbia.edu>
2018-02-13 17:03:25 -0800

    first

---
+++ true_random.py
@@ -0,0 +1,50 @@
+import requests
+from datetime import date,timedelta
+from PIL import Image
+import numpy as np
+import time
+
+start = date(2006,3,11)
+end = date.today()
+num_days = (end-start).days
+
+params = {'min':0,
+'max':num_days,
+'col':1,
+'rnd':'new',
+'format':'plain',
+'num':1,
+'base':10}
+
+try:
+	r = requests.get("https://www.random.org/integers", params)
+	data = int(r.content)
+except requests.exceptions.RequestException as e:
+    print e
+    sys.exit(1)
+
+new_date = start + timedelta(data)
+new_rnd = new_date.strftime('%Y-%m-%d')
+
+# bitmap
+params['rnd'] = new_rnd
+params['col'] = 3
+params['num'] = 128*16*3
+params['max'] = 255
+mat = []
+
+for _ in range(8):
+	try:
+		r = requests.get("https://www.random.org/integers", params)
+		data = r.content.splitlines()
+	except requests.exceptions.RequestException as e:
+	    print e
+	    sys.exit(1)
+	for line in data:
+		tup = tuple(map(int,line.split()))
+		mat.append(tup)
+	time.sleep(60)
+
+im = Image.new("RGB",(128,128))
+im.putdata(mat)
+im.save('img.jpeg')
